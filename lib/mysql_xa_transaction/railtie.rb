a7ffeac113568df3e0d77208d2eb52c6c99d8c11
Martijn Bolhuis <martijn.bolhuis@gmail.com>
2012-11-18 21:58:51 +0100

    Force rails logger flush on every addition

--- lib/mysql_xa_transaction/railtie.rb
+++ lib/mysql_xa_transaction/railtie.rb
@@ -5,7 +5,8 @@ require 'active_record_extensions.rb'
 
 class MysqlXARailtie < Rails::Railtie
   initializer "mysql_xa_railtie.configure_rails_initialization" do
-    config.xa_logger = Logger.new(STDOUT)
+    # Make sure that the log is immediately flushed to disk
+    Rails.logger.auto_flushing = true
     ActiveRecord::ConnectionAdapters::Mysql2Adapter.class_eval do
       alias_method :original_begin_db_transaction, :begin_db_transaction
       alias_method :original_commit_db_transaction, :commit_db_transaction
